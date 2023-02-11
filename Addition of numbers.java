d48719a8bcf98e0b26e36b4495de093f7c6ea163
Dhruva Bhat <dhruvabhat24@gmail.com>
2023-02-11 15:05:18 +0530

    Addition of integers

---
+++ Addition of numbers.java
@@ -0,0 +1,12 @@
+public class AdditionOfNumbers {
+	
+	public static void main(String []args) {
+		int a=10;
+		int b=20;
+		int c=45;
+		int d=94;
+		int z=23;
+		System.out.println(a+b+c+d+z);
+	}
+
+}
