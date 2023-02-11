974af72d8870df73aa70d28d22b135a27092e570
Dhruva Bhat <dhruvabhat24@gmail.com>
2023-02-11 17:12:16 +0530

    Perccentage

--- Percentage.java
+++ Percentage.java
@@ -7,21 +7,21 @@ public class UserInput1 {
 		System.out.println("The Result of the Student");
 		System.out.println("Marks are");
 		Scanner sc=new Scanner(System.in);
-		int sub1=sc.nextInt();
+		float sub1=sc.nextInt();
 		System.out.println("marks in sub 1:"+sub1);
-		int sub2=sc.nextInt();
+		float sub2=sc.nextInt();
 		System.out.println("marks in sub 2:"+sub2);
-		int sub3= sc.nextInt();
+		float sub3= sc.nextInt();
 		System.out.println("marks in sub 3:"+sub3);
-		int sub4=sc.nextInt();
+		float sub4=sc.nextInt();
 		System.out.println("marks in sub 4:"+sub4);
-		int sub5=sc.nextInt();
+		float sub5=sc.nextInt();
 		System.out.println("marks in sub 5:"+sub5);
-		int  total=sub1+sub2+sub3+sub4+sub5;
+		float  total=sub1+sub2+sub3+sub4+sub5;
 		System.out.println("Total marks is:"+total);
-		System.out.println("####Marks Card###");
-		System.out.println(sub1+"\n=sub 1\n"+sub2+"\n=sub 2\n"+sub3+"\n=sub 3\n"+sub4+"\n=sub 4\n"+sub5+"\n=sub 5\n");
-		System.out.println("Total=\n"+total);
+		float percentage=((sub1+sub2+sub3+sub4+sub5)/600)*100;
+		System.out.println(percentage);
 	}
 
 }
+
