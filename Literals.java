36fa22a4f6c50824bc1023564749bbcaad0a6d29
Dhruva Bhat <dhruvabhat24@gmail.com>
2023-02-11 15:28:03 +0530

    literals

---
+++ Literals.java
@@ -0,0 +1,24 @@
+public class Literals {
+	
+	public static void main(String []args) {
+		byte age=34;
+		int age2=56;
+		char ch='A';
+		long ageDino=56666666l;
+		float f1=5.6f;
+		double d1=4.66;
+		String a="Hi";
+		System.out.println(age);
+		System.out.println(age2);
+		System.out.println(ch);
+		System.out.println(ageDino);
+		System.out.println(f1);
+		System.out.println(d1);
+		System.out.println(a);
+	}
+
+} 
+// in this code '' , 'l' ,'f','D' are Literals
+// in order to declare a character type data type we need to write it within ''.
+// in order to declare a float 5.6f
+// double no need to add 'D'
