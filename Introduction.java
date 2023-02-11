c87cf23c6a9b4633c17c7368e06ab409f50e2276
Dhruva Bhat <dhruvabhat24@gmail.com>
2023-02-11 15:09:06 +0530

    Hello World

---
+++ Introduction.java
@@ -0,0 +1,23 @@
+public class Main {
+// main function
+	// package means:- package is used to group related classes
+	// Main is class
+	public static void main(String[] args) {
+		System.out.println("Hellow World");
+	}
+
+}
+// basic structure of java program 
+//function:- we group a similar kind of problems and we can use them whenever we need it 
+// classes:- classes is a collection of similar kind of functions.
+// program starts from 'main' function
+//public is access modifiers it means it can be accessed from anywhere else
+// static method allows the java program to run without an object
+//NAMIG CONVENTIONS
+// 1.for classes we use PascalConvention
+//  means the first letter in each word will be in 'caps'
+//example MyNameIsDhruv
+//for functions we use camelCaseConveention
+//first letter small case
+// AddTwoNbers-->Pascal naming convention
+//addTwoNumbers-->camel case naming convention
