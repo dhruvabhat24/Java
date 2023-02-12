0f7c89e74b8c0a13eb2328896170d602e93b5506
Dhruva Bhat <dhruvabhat24@gmail.com>
2023-02-12 10:51:06 +0530

    Operators

---
+++ Operatos.java
@@ -0,0 +1,16 @@
+public class Operators {
+
+	public static void main(String[] args) {
+		int a=4;
+		int b=6+a;
+		System.out.println(b);
+
+	}
+
+}
+// operators are used to operate on variables and values
+// Arithmetics operators--> +,-,*,%,/,++,--
+//Assignment operators--> =,+=
+//Comparison operator--> ==,>=,<=
+// Logical operators--> &&,||,!
+// Bitwise operator--> &,|
