00c2408484c452094aa27699db90a4142e2bbf55
Dhruva Bhat <dhruvabhat24@gmail.com>
2023-02-12 11:25:07 +0530

    Precedence in operators

---
+++ Precedence.java
@@ -0,0 +1,16 @@
+
+public class Precedence {
+
+	public static void main(String[] args) {
+		int a=6*5-34/2;
+		int b=6/5-34*2;
+		System.out.println(a);
+		System.out.println(b);
+		int c=1;
+		int d=4;
+		int e=7;
+		int k=c*c-4*e*c/2*e;
+		System.out.println(k);
+	}
+
+}
