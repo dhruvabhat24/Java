0c2c213928860d510272c41ed2dd1ec61f0e3517
Dhruva Bhat <dhruvabhat24@gmail.com>
2023-02-13 16:06:55 +0530

    Strings

---
+++ String.java
@@ -0,0 +1,14 @@
+
+
+public class Strings {
+
+	public static void main(String[] args) {
+		String name=new String("Dhruva");
+		String name1="Dilip";
+		System.out.println(name);
+		System.out.println(name1);
+		
+
+	}
+
+}
