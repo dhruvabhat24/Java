a01482e95a5030b6b87eeda9616e3078a2a6ece5
Dhruva Bhat <dhruvabhat24@gmail.com>
2023-02-13 15:22:24 +0530

    Increment and Decrement Operator
    In this code you are able to see how the increment and decrement operators work.

---
+++ Increment and Decrement.java
@@ -0,0 +1,22 @@
+
+public class IncrementDecrement {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        int i=56;
+        char a='C';
+        char k='O';
+		System.out.println(i++);//first i is used and then incremented
+		System.out.println(i);//as i is incremented in previous print function here i=57
+		System.out.println(++i);// first i will be incremented and then it will be used so i=58
+		System.out.println(i);// here i =58
+		System.out.println(a);
+		System.out.println(++a);
+		System.out.println(k);
+		System.out.println(k--);
+		System.out.println(--k);
+		System.out.println(--i);
+	}
+
+}
+// decrement operator will works on the same rule 
