417cd110adb327b1d803fa020bee545b1257f364
Dhruva Bhat <dhruvabhat24@gmail.com>
2023-02-13 16:20:38 +0530

    Different printing methods in JAVA

---
+++ print.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class DiffPrint {
+
+	public static void main(String[] args) {
+		System.out.print("hi");
+		System.out.println("Hello");
+		int a= 4;
+			float b=5.6574f;
+	System.out.printf("the value of a is %d and the value of b is %f",a,b);
+		Scanner sc=new Scanner(System.in);
+		String st1=sc.nextLine();
+		System.out.println(st1);
+	}
+
+}
