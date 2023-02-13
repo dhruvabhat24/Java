9f2a67e0fb9f4163d85f070db0e0c9bf8dcd2de7
Dhruva Bhat <dhruvabhat24@gmail.com>
2023-02-13 15:38:30 +0530

    Result

--- Result.java
+++ Result.java
@@ -1,9 +1,12 @@
+package com.practice;
+
 public class Result {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-        float a=7/4*9/2;
+        float a=7/4.0f*9/2.0f;
         System.out.println(a);
 	}
 
 }
+
