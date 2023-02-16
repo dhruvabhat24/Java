7175cc728233dcfaabde74f102bc85bc71a575cc
Dhruva Bhat <dhruvabhat24@gmail.com>
2023-02-16 16:05:04 +0530

    Do-While

---
+++ Do-While.java
@@ -0,0 +1,19 @@
+
+public class DoWhile {
+
+	public static void main(String[] args) {
+		int a=2;
+		while(a<5)
+		{
+			System.out.println("The value of a is "+a);
+			a++;
+		}
+		// in do while it will first execute the code and checks the condition 
+		int b=0;
+      do {
+    	  System.out.println(b);
+    	  b++;
+      }while(b<5);
+	}
+
+}
