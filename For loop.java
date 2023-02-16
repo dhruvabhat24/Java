f642905201a5ecc124fae42ad27c1da5bf614e24
Dhruva Bhat <dhruvabhat24@gmail.com>
2023-02-16 16:18:14 +0530

    For Loops in Java

---
+++ For loop.java
@@ -0,0 +1,19 @@
+
+public class ForLoop {
+
+	public static void main(String[] args) {
+		//for (int i=0;i<10;i++)
+		//{
+		//	System.out.println(i);
+		//}
+		// write a program to print sum of first 10 odd
+		//2n = even number=0,2,4,6,8
+		// 2n+1=odd numbers=1,3,5,7,9
+		int n=5;
+		for(int i=0;i<5;i++)
+		{
+			System.out.println(2*i+1);
+		}
+	}
+
+}
