58c0b7f2e4ac92fe4427b8efb45687f4f5d38eb5
Dhruva Bhat <dhruvabhat24@gmail.com>
2023-02-16 15:54:00 +0530

    While Loop

---
+++ While Loops.py
@@ -0,0 +1,19 @@
+
+public class WhileLoop {
+
+	public static void main(String[] args) {
+		System.out.println(1);
+		System.out.println(2);
+		System.out.println(3);
+		System.out.println("Using Loops");
+		int i=1;
+		while(i<=3)
+		{
+			System.out.println(i);
+			i++;
+		}
+			System.out.println("While loop ended");
+	}
+
+}
+// if the while loop condition will run till its condition becomes false 
