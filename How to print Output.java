ebcd494157170fae9b0a9ab402b160e72a324eba
Dhruva Bhat <dhruvabhat24@gmail.com>
2023-02-27 17:02:31 +0530

    How to print Output

---
+++ How to print Output.java
@@ -0,0 +1,15 @@
+
+class Codechef
+{
+	public static void main (String[] args)
+	{
+		int a = 5;
+		System.out.println(a);
+		
+		String b = "abcde";
+		System.out.println(b);
+		
+		int[] array = {1, 2, 3};
+		System.out.println(array[0] + " " + array[1] + " " + array[2]);
+	}
+}
