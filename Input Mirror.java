4ef18dc1095914b4de54ce191358841c6ce9c6a7
Dhruva Bhat <dhruvabhat24@gmail.com>
2023-02-28 14:42:59 +0530

    Input Mirror

---
+++ Input Mirror.java
@@ -0,0 +1,14 @@
+
+import java.util.Scanner;
+
+class Codechef
+{
+	public static void main (String[] args)
+	{
+        Scanner read = new Scanner(System.in);
+        int N = read.nextInt();
+        String S = read.next();
+		System.out.print(N + " ");
+		System.out.print(S);
+	}
+}
