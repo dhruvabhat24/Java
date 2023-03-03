e2448918617e65fa8ed9c48b36997327b0955c60
Dhruva Bhat <dhruvabhat24@gmail.com>
2023-03-03 21:36:21 +0530

    Create Test case with multiple lines.java

---
+++ Test case with multiple lines.java
@@ -0,0 +1,25 @@
+// Solution as follows
+import java.util.Scanner;
+
+class Codechef
+{
+	public static void main (String[] args)
+	{
+        Scanner read = new Scanner(System.in);
+        // accept the count of test cases given in the the 1st line
+        int t = read.nextInt();
+        
+        // Run a loop to accept 't' inputs
+        for(int i=0; i<t; i++)
+        {
+            // accept 5 integers in each test case
+            int a = read.nextInt();
+            int b = read.nextInt();
+            int c = read.nextInt();
+            int d = read.nextInt();
+            int e = read.nextInt();
+            // output the 5 integers on a single line for each test case
+            System.out.println(a + " " + b + " " + c + " " + d + " " + e);
+        }
+}
+}
