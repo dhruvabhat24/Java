97af6ef6a839c955f11d0c924e207c6cff03167e
Dhruva Bhat <dhruvabhat24@gmail.com>
2023-03-05 20:39:27 +0530

    Difference between Run and Submit

---
+++ Difference between Run and Submit.java
@@ -0,0 +1,17 @@
+
+import java.util.Scanner;
+
+class Codechef
+{
+	public static void main (String[] args)
+	{
+        Scanner read = new Scanner(System.in);
+        
+        int t = read.nextInt();
+        for(int i=0; i<t; i++)
+        {
+            int n = read.nextInt();
+            System.out.println(n+1);
+        }
+	}
+}
