ff33b506b83d472c582f551506196c47bd0b07db
Dhruva Bhat <dhruvabhat24@gmail.com>
2023-03-18 19:52:42 +0530

    Some useful math functions

---
+++ Math Functions.java
@@ -0,0 +1,24 @@
+class Codechef
+{
+	public static void main (String[] args)
+	{
+		int a = 45;
+		int b = 36;
+    	
+    	// Output the minimum value of 'a' and 'b'
+		System.out.println(Math.min(a, b));
+		
+		// Output the minimum value of 'a' and 'b'
+		System.out.println(Math.max(a, b));
+		
+		// Output the absolute value of the difference of 'b' and 'a'
+		System.out.println(Math.abs(b - a));
+		
+		int c = 6;
+        int d = 8;
+        
+        // Output the remainder when 'b' is divided by 'c' and 'd'
+        System.out.println(b % c);
+        System.out.println(b % d);
+	}
+}
